030381107b9281041a744796e2d86046f046dad5
Rohit Rawat <75693046+rohitrawatcse@users.noreply.github.com>
2023-09-05 03:06:20 +0530

    Add files via upload

---
+++ src/utils/moviesSlice.js
@@ -0,0 +1,38 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const moviesSlice = createSlice({
+  name: "movies",
+  initialState: {
+    nowPLayingMovies: null,
+    trailerVideo: null,
+    popularMovies: null,
+    topRatedMovies: null,
+    upcomingMovies: null,
+  },
+  reducers: {
+    addNowPlayingMovies: (state, action) => {
+      state.nowPLayingMovies = action.payload;
+    },
+    addPopularMovies: (state, action) => {
+      state.popularMovies = action.payload;
+    },
+    addTrailerVideo: (state, action) => {
+      state.trailerVideo = action.payload;
+    },
+    addTopRatedMovies: (state, action) => {
+      state.topRatedMovies = action.payload;
+    },
+    addUpcomingMovies: (state, action) => {
+      state.upcomingMovies = action.payload;
+    },
+  },
+});
+
+export const {
+  addNowPlayingMovies,
+  addTrailerVideo,
+  addPopularMovies,
+  addTopRatedMovies,
+  addUpcomingMovies,
+} = moviesSlice.actions;
+export default moviesSlice.reducer;
